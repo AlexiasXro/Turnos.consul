f515e30452dc485aa67b24fa081135b23ba14771
Alejandra Caceres <caceres299romina@hotmail.com>
2025-10-31 15:30:44 -0300

    mensaje del cambio

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,3 +1,5 @@
+// con children: los componentes hijos que se pasen a Container
+
 export default function Container({ children }) {
   return (
     <div style={{ maxWidth: '600px', margin: '0 auto', padding: '2rem' }}>
